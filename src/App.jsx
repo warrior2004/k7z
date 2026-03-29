332cc0d2eb96f8318a15d7b7415bcf57d360ad21
Harshit Chauhan <harshit.honey003@gamil.com>
2026-03-29 14:31:59 +0530

    Added Page transtion logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Agency from './pages/Agency'
 import Projects from './pages/Projects'
+import './App.css'
 
+const Stairs = () => (
+  <div className='stairs-container'>
+    <div className='stair stair-1'></div>
+    <div className='stair stair-2'></div>
+    <div className='stair stair-3'></div>
+    <div className='stair stair-4'></div>
+    <div className='stair stair-5'></div>
+  </div>
+)
 
 const App = () => {
+  const [isAnimating, setIsAnimating] = useState(false)
+  const location = useLocation()
+
+  useEffect(() => {
+    setIsAnimating(true)                          // show stairs on route change
+    setTimeout(() => setIsAnimating(false), 1500) // hide after 1.5 seconds
+  }, [location.pathname])                         // triggers on every route change
+
   return (
     <div>
+      {isAnimating && <Stairs />}
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/agency' element={<Agency />}/>
--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import './Agency.css'
+import { useState, useEffect } from "react";
+import "./Agency.css";
+
+const images = [
+  "Images/image1.jpg",
+  "Images/image2.jpg",
+  "Images/image3.jpg",
+  "Images/image4.jpg",
+  "Images/image5.jpg"
+]
 
 const Agency = () => {
-  return (
-    <div>Agency</div>
-  )
-}
+  const [currentImage, setCurrentImage] = useState(0);
 
-export default Agency
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prev) => (prev+1) % images.length);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="agency-container">
+      <div className="image">
+        <img src={images[currentImage]} alt="" />
+      </div>
+      <div className="heading1">
+        <h1> Soixan7e</h1>
+        <h1>Douze</h1>
+      </div>
+      <div className="paragraph">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio,
+          deserunt fugit doloremque architecto quam enim suscipit molestias
+          repellendus hic voluptatem quaerat! Dolore, adipisci ratione iste
+          totam placeat at eligendi ea quae natus ipsum fugit nemo labore maxime
+          modi quia vel maiores voluptatum perspiciatis amet odit reprehenderit
+          aliquid esse qui. Maxime!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Agency;
